3f7d9fe7d0b959b95c7a8514a003e6a72850f879
pjhuang <pjhuang+github@gmail.com>
2014-11-03 13:48:22 +0800

    Initial import of scm_info
    It's a script used to retrieve SCM information

---
+++ scm_info/scm_info.h
@@ -0,0 +1,6 @@
+/* This file was generated/updated by ROJECT_ROOT/tools/setlocalversion
+ * script file automatically. Typically, the script will be invoked at the
+ * build time.
+ */
+#define ATOP_SCM_VERSION "-svn14-dirty7"
+#define ATOP_SCM_URL "http://10.0.0.185/repos/LinuxAM335x/branches/simonhuang_se5901_sw_int"
